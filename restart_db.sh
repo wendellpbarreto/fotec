3a4c4464a1c7e184d564cbacb4fc3bcd130ff975
Wendell P. Barreto <wendellp.barreto@gmail.com>
2014-01-15 20:29:38 -0300

    Initial commit.

---
+++ restart_db.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#-----------------------------------
+# @autor: Wendell P. Barreto
+# @email: wendellp.barreto@gmail.com
+# @project: fotec
+# @doc: restart_db.sh
+# ----------------------------------
+
+
+while true; do
+    read -p "Are you using Linux (y or n)? " yn
+    case $yn in
+        [Yy]* )
+        	sudo -u postgres psql -c 'DROP DATABASE fotec_db'
+			sudo -u postgres psql -c 'CREATE DATABASE fotec_db'
+			sudo -u postgres psql -c 'CREATE USER fotec_admin'
+			sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE fotec_db TO fotec_admin'
+			sudo -u postgres psql -d fotec_db -c 'CREATE EXTENSION hstore' 
+
+			break;;
+        [Nn]* ) 
+			psql -c 'DROP DATABASE fotec_db'
+			psql -c 'CREATE DATABASE fotec_db'
+			psql -c 'CREATE USER fotec_admin'
+			psql -c 'GRANT ALL PRIVILEGES ON DATABASE fotec_db TO fotec_admin'
+			psql -d fotec_db -c 'CREATE EXTENSION hstore'
+
+			break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+python manage.py syncdb
+python manage.py collectstatic
